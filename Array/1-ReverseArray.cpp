fa1a8df1aae42446c279bb05a2e5d4b653ce41d3
Kshitij21jain <jainkshitijshah07@gmail.com>
2024-10-21 09:48:05 +0530

    Array reverse file modified

---
+++ Array/1-ReverseArray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+void ReverseArray(int arr[], int n, int i){
+    if(i>=n/2) return;
+    swap(arr[i], arr[n-1]);
+    ReverseArray(arr,n,i++);
+
+}
+void print(int arr[], int n){
+    for(int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+    ReverseArray(arr,n,0);
+    print(arr,n);
+    return 0;
+}
